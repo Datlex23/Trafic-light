3b1ca41611aa7f92919b8ab050a508714ba12e82
Doan Tuan Anh <dat_life94@mail.ru>
2023-03-05 20:40:18 +0700

    Work with UI

--- Trafic light/ViewController.swift
+++ Trafic light/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    
+    @IBOutlet var actionButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = 40
+        yellowLightView.layer.cornerRadius = 40
+        greenLightView.layer.cornerRadius = 40
+        actionButton.layer.cornerRadius = 10
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+    
+        
     }
 
-
+    @IBAction func actionButtonDidTapped() {
+        
+        redLightView.alpha = 1
+        redLightView.alpha = 0.3
+    }
+    
 }
 
