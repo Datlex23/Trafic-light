0766c95a44b9320024b644517880692ed639d26f
Doan Tuan Anh <dat_life94@mail.ru>
2023-03-05 19:15:53 +0700

    Initial Commit

---
+++ Trafic light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trafic light
+//
+//  Created by Doan Tuan Anh on 05/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
